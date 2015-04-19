f19dc4e0ff69f246777876fe6fb3857553546fc4
J-Nunes <jordinunes96@gmail.com>
2015-04-19 22:11:00 +0200

    Iterative Tree

--- Project1/Project1/Tree.h
+++ Project1/Project1/Tree.h
@@ -1,22 +1,49 @@
 #ifndef __Tree_H__
 #define __Tree_H__
 
-#include "Doublelist.h"
+#include "Dlist.h"
+#include "Stack.h"
 
 template<class TYPE>
 struct TreeNode
 {
 	TYPE data;
 	TreeNode<TYPE>* parent;
-	DoubleList<TreeNode*> sons;
+	DList<TreeNode*> sons;
 	
-	//--------PRE ORDER--------
-		void visitAll(DoubleList<TYPE>* list);
+	void preOrder(DList<TYPE>* list);
+	{
+		list.add(data);
+
+		DListNode<TreeNode*>* tmp = sons.start;
+
+		for (; tmp!=NULL; tmp=tmp->next)
+			tmp->value->preOrder(list);
+	}
+
+	void postOrder(DList<TYPE>* list);
+	{
+		DListNode<TreeNode*>* tmp = sons.start;
+
+		for (; tmp != NULL; tmp = tmp->next)
+			tmp->value->postOrder(list);
+		
+		list.add(data);
+	}
+
+	void inOrder(DList<TYPE>* list);
+	{
+		DListNode<TreeNode*>* tmp = sons.start;
+
+		for (; tmp != NULL; tmp = tmp->next)
 		{
-			list.Add(data);
-			for (unsigned int i = 0; i < sons.count(); i++)
-				visitAll(list);
+			if (tmp != sons.start)
+			list.add(data);
+
+			tmp->value->inOrder(list);
 		}
+
+		list.add(data);
 };
 
 
@@ -53,12 +80,110 @@ class Tree
 			return newNode;
 		}
 
-		//--------PRE ORDER--------
-		void visitAllNodes(DoubleList<TYPE>* list)const;
+		void recursivePreOrder(DList<TYPE>* list)const;
 		{
-		rootNode.visitAll(list);
+			rootNode.preOrder(list);
 		}
 		
+		void recursivePostOrder(DList<TYPE>* list)const;
+		{
+			rootNode.postOrder(list);
+		}
+
+		void recursiveInOrder(DList<TYPE>* list)const;
+		{
+			rootNode.inOrder(list);
+		}
+
+		void iterativePreOrder(DList<TYPE>* list)const;
+		{
+			Stack<TreeNode> s;
+			TreeNode<TYPE>* tmpNode = rootNode;
+
+			while (tmpNode != NULL)
+			{
+				list.add(tmpNode);
+				DListNode<TreeNode>* tmp = sons.end;
+
+				while (tmp != NULL)
+				{
+					s.push(tmp->data);
+					tmp = tmp->prev;
+				}
+				tmpNode = s.pop;
+			}
+		}
+
+		void iterativePostOrder(DList<TYPE>* list)const;
+		{
+			Stack<TreeNode> s;
+			TreeNode<TYPE>* tmpNode = rootNode;
+
+			while (tmpNode != NULL)
+			{
+				if (tmpNode->sons.start != NULL && list->end != tmpNode->sons.end)
+				{
+						s.push(tmpNode);
+
+						DListNode<TreeNode>* tmp = sons.end;
+
+						while (tmp != NULL)
+						{
+							s.push(tmp->data);
+							tmp = tmp->prev;
+						}
+						tmpNode = s.pop;
+				}
+
+				else
+				{
+					list.add(tmpNode);
+					tmpNode = s.pop;
+				}
+			}
+			
+		}
+
+		void iterativeInOrder(DList<TYPE>* list)const;
+		{
+			Stack<TreeNode> s;
+			TreeNode<TYPE>* tmpNode = rootNode;
+
+			while (tmpNode != NULL)
+			{
+				if (tmpNode->sons.start != NULL)
+				{
+					if (list->end != tmpNode->sons.start)
+					{
+						s.push(tmpNode);
+						s.push(tmp->sons.start);
+						tmpNode = s.pop;
+					}
+
+					else
+					{
+						if (list->end == tmpNode->sons.end)
+						{
+							list.add(tmpNode);
+							tmpNode = s.pop;
+						}
+
+						else
+						{
+							list.add(tmpNode);
+							s.push(tmp->sons.end);
+							tmpNode = s.pop;
+						}
+					}
+				}
+
+				else
+				{
+					list.add(tmpNode);
+					tmpNode = s.pop;
+				}
+			}
+		}
 };
 
 #endif // !__Tree_H__
